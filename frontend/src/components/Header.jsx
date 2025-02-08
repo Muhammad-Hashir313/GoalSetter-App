d6bc4c60060cd28ceae515af9b1c66d5d5a45e1e
Muhammad Hashir <mehashir313@gmail.com>
2025-02-08 16:36:07 +0500

    Added Frontend authentication UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,50 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout, reset } from '../features/auth/authSlice'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Header = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user } = useSelector(state => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
     return (
         <header className='header'>
             <div className="logo">
                 <Link to='/'>GoalSetter</Link>
             </div>
             <ul>
-                <li>
-                    <Link to='/login'>
-                        <FaSignInAlt /> Login
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/register'>
-                        <FaUser /> Register
-                    </Link>
-                </li>
-            </ul>
+                {user ? (
+                    <li>
+                        <button onClick={onLogout} className='btn'>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/login'>
+                                <FaSignInAlt /> Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/register'>
+                                <FaUser /> Register
+                            </Link>
+                        </li>
+                    </>
+                )}
 
-        </header>
+            </ul >
+
+        </header >
     )
 }
 
